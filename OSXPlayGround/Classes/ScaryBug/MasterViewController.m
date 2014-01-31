6f81b1747f6f5c2c9e3d7c78a29beb7a13748e03
Jenei Viktor <jeneiv@gmail.com>
2014-01-31 16:40:41 +0100

    - ScaryBug TableView Controller imeplementation - UI adjusted to handle table view + cell configuration

--- OSXPlayGround/Classes/ScaryBug/MasterViewController.m
+++ OSXPlayGround/Classes/ScaryBug/MasterViewController.m
@@ -7,20 +7,50 @@
 //
 
 #import "MasterViewController.h"
+#import "ScaryBugDoc.h"
+#import "ScaryBugData.h"
 
 @interface MasterViewController ()
+
+@property (strong) NSMutableArray *bugs;
 
 @end
 
 @implementation MasterViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Initialization code here.
+        ScaryBugDoc *bug1 = [[ScaryBugDoc alloc] initWithTitle:@"Potato Bug" rating:4 thumbImage:[NSImage imageNamed:@"potatoBugThumb.jpg"] fullImage:[NSImage imageNamed:@"potatoBug.jpg"]];
+        ScaryBugDoc *bug2 = [[ScaryBugDoc alloc] initWithTitle:@"House Centipede" rating:3 thumbImage:[NSImage imageNamed:@"centipedeThumb.jpg"] fullImage:[NSImage imageNamed:@"centipede.jpg"]];
+        ScaryBugDoc *bug3 = [[ScaryBugDoc alloc] initWithTitle:@"Wolf Spider" rating:5 thumbImage:[NSImage imageNamed:@"wolfSpiderThumb.jpg"] fullImage:[NSImage imageNamed:@"wolfSpider.jpg"]];
+        ScaryBugDoc *bug4 = [[ScaryBugDoc alloc] initWithTitle:@"Lady Bug" rating:1 thumbImage:[NSImage imageNamed:@"ladybugThumb.jpg"] fullImage:[NSImage imageNamed:@"ladybug.jpg"]];
+        _bugs = [NSMutableArray arrayWithObjects:bug1, bug2, bug3, bug4, nil];
     }
     return self;
 }
 
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    
+    // Get a new ViewCell
+    NSTableCellView *cellView = [tableView makeViewWithIdentifier:tableColumn.identifier owner:self];
+    
+    // Since this is a single-column table view, this would not be necessary.
+    // But it's a good practice to do it in order by remember it when a table is multicolumn.
+    if( [tableColumn.identifier isEqualToString:@"BugColumn"] )
+    {
+        ScaryBugDoc *bugDoc = [self.bugs objectAtIndex:row];
+        cellView.imageView.image = bugDoc.thumbImage;
+        cellView.textField.stringValue = bugDoc.data.title;
+        return cellView;
+    }
+    return cellView;
+}
+
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    return [self.bugs count];
+}
 @end
